6fea6150e5aed2082551f3766832f41d6dc8ea75
reznik <reznik@mail.ru>
2018-12-04 15:55:52 +0200

    create initial architecture

---
+++ src/main/java/com/codingbattle/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.codingbattle.controller;
+
+import com.codingbattle.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.codingbattle.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable("userId") String id) {
+        return userRepository.findOne(id);
+    }
+
+    @PostMapping
+    public User saveUser(@RequestParam(name = "userId") String id) {
+        return userRepository.save(new User(id));
+    }
+
+}
